8f44ccb58009887d089013819b648b4db50140bf
Shreyansh Singh <sshreyansh336@gmail.com>
2024-10-11 12:46:21 +0530

    Python Basics

---
+++ Introduction.py
@@ -0,0 +1,68 @@
+print("hello world")
+
+
+print("Shreyansh Singh")
+
+print("Shreyansh is my name and my age is 20")
+
+print(23)
+
+print(23+23)
+
+name="shreyansh"
+print(name)
+
+
+age=20
+print(age)
+
+
+price=25.99
+print(price)
+
+print("my name is :", name)
+print("my age is :", age)
+
+age2 = age 
+print(age2)
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+
+name1="SS"
+name2='SS'
+name3='''SS'''
+
+print(name1)
+print(name2)
+print(name3)
+
+age=23
+old=False
+a=None
+print(type(old))
+print(type(a))
+
+a=2
+b=1000
+sum=a+b
+print(sum)
+
+a=1000
+b=500
+diff=a-b
+print(diff)
+
+#Hi , I am a comment
+
+"""
+Multi 
+line 
+comment
+
+"""
+#If i want to highlight many line just use ctrl+/
+
+
