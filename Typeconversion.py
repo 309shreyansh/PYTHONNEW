d1df6680f18c6e279f57cbe0ac86ddc25e877ba4
Shreyansh Singh <sshreyansh336@gmail.com>
2024-10-13 22:03:38 +0530

    About type conversion

---
+++ Typeconversion.py
@@ -0,0 +1,34 @@
+#type conversion
+
+a=2
+b=4.25
+
+sum=a+b # 2.0 +4.25 => 6.25
+
+print(sum)
+
+"""if 2 would have been written in "" then
+
+the addition could not have been
+possible because it would have been a string value
+
+"""
+
+a=int("2")
+b=4.25
+
+print(a+b)
+
+a=float("2")
+b=4.25
+
+print(a+b)
+
+
+#NOTE
+
+a=3.14
+a=str(a)
+print(type(a))
+
+
