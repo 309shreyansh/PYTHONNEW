ed89a70cfb8b14f546e32e95057e95f8e547df85
Shreyansh Singh <sshreyansh336@gmail.com>
2024-10-15 10:32:50 +0530

    About input

---
+++ Input.py
@@ -0,0 +1,14 @@
+val=input("enter some value:")
+print(type(val),val)
+
+name=input("enter your name:")
+print("welcome", name)
+
+
+name=input("enter name:")
+age=input("enter age:")
+marks=input("enter marks:")
+
+print("welcome",name)
+print("age=",age)
+print("marks=",marks)
